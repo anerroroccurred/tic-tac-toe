3436fbcec5314cb02bec67fb0c5061c10a5e530e
anerroroccurred <404.anerroroccurred@gmail.com>
2016-07-28 19:06:15 +0200

    Add cross planting script

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
-/**
- * Created by admin on 2016-07-28.
- */
+$(function() {
+    $(".field").click(function() {
+        $(this).html('<i class="fa fa-times fa-5" aria-hidden="true"></i>');
+    });
+    $("#clear-button").click(function() {
+        $(".field").each(function() {
+            $(this).empty();
+        });
+    });
+});
